94a1933c2bd27c111846a7f50b5f88aca75f4a17
BetterLordWilliam <willjordan05@icloud.com>
2026-01-27 21:16:45 -0800

    WO: dynamic

--- src/main.py
+++ src/main.py
@@ -55,13 +55,20 @@ class VideoApp(App):
 
 
 if __name__ == '__main__':
-    # main()
-
     """
     spin up a seperate thread to process the video
 
     make it call a method in that clears the log & flushes the
     frame to the log via write lines
+
+    actually there are built-in textual workers that can be used for this purpose...
+
+    TODO :    
+
+    Questions that need answering
+    - scale the video while it is being procesed (like per frame of the generators execution)?
+    - streaming video from insta reels into the converter
+    - the rest of the interface, reels copy but in the terminal
 
     """
 
@@ -70,10 +77,9 @@ if __name__ == '__main__':
         description='this program lets you scroll brainrot in the terminal'
     )
     args.add_argument('-v', '--video-path',
-        help="video path, ex. \'/my/dir/video.p4\'",
+        help="video path, ex. \'/my/dir/video.mp4\'",
         required=True
     )
-
     pargs = args.parse_args()
 
     video_path = Path(pargs.video_path)
